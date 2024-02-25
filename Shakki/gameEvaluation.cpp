60b452274b2e0f8eb07861e209cb0bc56aac589b
Mursu <mursujesse@gmail.com>
2024-02-25 10:45:09 +0200

    improved move legality function

--- Shakki/gameEvaluation.cpp
+++ Shakki/gameEvaluation.cpp
@@ -4,6 +4,12 @@
 
 Evaluation eval = Evaluation();
 
+/*
+MinimaxValue GameState::iterative_deepening(TranspositionTable& tt) const
+{
+	return;
+}
+*/
 MinimaxValue GameState::minimax(int depth, int alpha, int beta, TranspositionTable& tt) const
 {
 	// Generate moves for this state
@@ -31,7 +37,7 @@ MinimaxValue GameState::minimax(int depth, int alpha, int beta, TranspositionTab
 		return MinimaxValue(evaluate(), Move());
 	}
 	
-
+	
 	// TODO: Calculate zobrist keys without test state, 
 	for (Move& m : moves)
 	{
@@ -47,7 +53,7 @@ MinimaxValue GameState::minimax(int depth, int alpha, int beta, TranspositionTab
 		}
 	}
 	
-	bool isMin = TurnPlayer == WHITE ? true : false;
+	bool isMax = TurnPlayer == WHITE ? true : false;
 
 	if (TurnPlayer == BLACK)
 	{
@@ -57,13 +63,12 @@ MinimaxValue GameState::minimax(int depth, int alpha, int beta, TranspositionTab
 	{
 		sort(moves.begin(), moves.end(), greater<>());
 	}
-
+	
 	// Get the best_value for player
 	int best_value = TurnPlayer == WHITE ?
 		numeric_limits<int>::lowest() : numeric_limits<int>::max();
 
 	Move best_move(0, 0, 0, 0);
-
 	// Iterate through moves
 	for (Move& m : moves)
 	{
@@ -71,58 +76,54 @@ MinimaxValue GameState::minimax(int depth, int alpha, int beta, TranspositionTab
 		GameState new_state = *this;
 		new_state.make_move(m);
 
-		// for better TT saving eval in node and best move in node
-		Move best_move_in_node(0, 0, 0, 0);
-		int best_value_in_node = TurnPlayer == WHITE ? numeric_limits<int>::lowest() : numeric_limits<int>::max();
+		// Check move legality during search to avoid unnecessarily checking moves
+		// that will be pruned away by alpha beta
 
-		// Check move legality during search to avoid unnecessary checks. 
-		// Still using inefficient method.
-		if (!is_legal(m)) continue;
+		/* old expensive method here only for testing
+		auto king_pos = (TurnPlayer == WHITE) ? new_state._wK_pos : new_state._bK_pos;
+		if (new_state.is_under_threat(king_pos[0], king_pos[1], 1 - TurnPlayer)) continue;
+		*/
+
+		// new method to check if king is in check. extremely fast
+		if (new_state.is_king_in_check(TurnPlayer)) continue;
 		legal_moves_made++;
 
 		// Recursive call
 		MinimaxValue value = new_state.minimax(depth - 1, alpha, beta, tt);
 
 		// Get best value for player
-		if ((isMin && value.Value > best_value) ||
-			(!isMin && value.Value < best_value))
+		if ((isMax && value.Value > best_value) ||
+			(!isMax && value.Value < best_value))
 		{
 			best_value = value.Value;
 			best_move = m;
 			
-			if (isMin)
+			if (isMax && best_value > alpha)
 			{
 				alpha = best_value;
 			}
-			else
+			else if (!isMax && best_value < beta)
 			{
 				beta = best_value;
 			}
 			
 		}
-
-		// update best value in node
-		if (value.Value > best_value_in_node)
-		{
-			best_value_in_node = value.Value;
-			best_move_in_node = m;
-		}
-
+		tt._positionCount++;
 		// store position to TT
-		tt.hash_new_position(new_state, depth, best_value_in_node, best_move_in_node);
-		
+		tt.hash_new_position(new_state, depth - 1, value.Value, best_move);
 		if (beta <= alpha)
 		{
 			break;
 		}
 		
 	}
+
+	// tt.hash_new_position(*this, depth, best_value, best_move);
 	// no legal moves in branch, game is over
 	if (legal_moves_made <= 0)
 	{
 		return	MinimaxValue(score_board(), Move());
 	}
-
 	return MinimaxValue(best_value, best_move);
 }
 
--- Shakki/gameMoves.cpp
+++ Shakki/gameMoves.cpp
@@ -217,23 +217,205 @@ void GameState::get_moves(vector<Move>& moves) const
 	moves.erase(moves.begin() + moveIndex, moves.end());
 }
 
-bool GameState::is_legal(Move& m) const {
-	GameState testState = *this;
-	testState.make_move(m);
+bool GameState::is_diagonal_safe(int row_direction, int column_direction, int row, int column, int player) const {
+	int current_row = row;
+	int current_column = column;
+	current_column += column_direction;
+	current_row += row_direction;
+	int piece;
+	int steps = 0;
+	int pawn_attack_check_direction = (player == WHITE) ? -1 : 1;
 
+	// loop all tiles in direction
+	while (current_column <= 7 && current_column >= 0 && current_row <= 7 && current_row >= 0)
+	{
+		steps++;
+		piece = _board[current_row][current_column];
+		if (piece != NA)
+		{
+			// if piece is same colour king is safe as any attack would be blocked by this piece
+			if (get_piece_color(piece) == player) return true;
+
+			// if only one step is taken towards opponents side diagonally and piece is pawn
+			// we are in check
+			if (steps == 1) {
+				if (piece == bP && pawn_attack_check_direction == -1) return false;
+				if (piece == wP && pawn_attack_check_direction == 1) return false;
+			}
+			// if piece is queen or bishop king is not safe
+			if (piece == wQ || piece == bQ || piece == wB || piece == bB) return false;
+
+			// if piece is anything else king is safe as they cannot attack diagonally
+			return true;
+		}
+		current_column += column_direction;
+		current_row += row_direction;
+	}
+	// if no pieces were hit during loop king is safe
+	return true;
+}
+
+
+bool GameState::is_row_or_column_safe(int row_direction, int column_direction, int row, int column, int player) const {
+	int current_column = column;
+	int current_row = row;
+	current_column += column_direction;
+	current_row += row_direction;
+	int piece;
+
+	// loop all tiles in direction
+	while (current_column <= 7 && current_column >= 0 && current_row <= 7 && current_row >= 0)
+	{
+		piece = _board[current_row][current_column];
+		if (piece != NA)
+		{
+			// if piece is same colour king is safe as any attack would be blocked by this piece
+			if (get_piece_color(piece) == player) return true;
+
+			// if piece is queen or rook king is not safe
+			if (piece == wQ || piece == bQ || piece == wR || piece == bR) return false;
+
+			// if piece is anything else king is safe
+			return true;
+		}
+		current_column += column_direction;
+		current_row += row_direction;
+	}
+	// if no pieces were hit during loop king is safe
+	return true;
+}
+
+bool GameState::is_safe_from_knight(int row, int column, int player) const {
+	// check for horse moves.
+	int enemy_horse = (player == WHITE) ? bN : wN;
+	int current_row;
+	int current_column;
+	for (int i = -1; i < 2; i += 2)
+	{
+		for (int j = -1; j < 2; j += 2)
+		{
+			current_row = row + j;
+			current_column = column + i * 2;
+			if (current_row < 0 || current_row > 7 || current_column < 0 || current_column > 7) continue;
+			if (_board[current_row][current_column] == enemy_horse) return false;
+		}
+		for (int j = -1; j < 2; j += 2)
+		{
+			current_row = row + (i * 2);
+			current_column = column + j;
+			if (current_row < 0 || current_row > 7 || current_column < 0 || current_column > 7) continue;
+			if (_board[current_row][current_column] == enemy_horse) return false;
+		}
+	}
+	return true;
+}
+
+bool GameState::is_king_in_check(int player) const {
 	// Get king position
 	int row, column;
-	if (TurnPlayer == WHITE)
+	if (player == WHITE)
 	{
-		row = testState._wK_pos[0];
-		column = testState._wK_pos[1];
+		row = _wK_pos[0];
+		column = _wK_pos[1];
 	}
 	else
 	{
-		row = testState._bK_pos[0];
-		column = testState._bK_pos[1];
+		row = _bK_pos[0];
+		column = _bK_pos[1];
 	}
-	return !testState.is_under_threat(row, column, 1 - TurnPlayer);
+	// checkk all possible tiles where knight can attack king
+	if (!is_safe_from_knight(row, column, player)) return true;
+
+	// check horizontal and vertical moves for attacks from rooks and quuens
+	if (!is_row_or_column_safe(1, 0, row, column, player)) return true;
+	if (!is_row_or_column_safe(-1, 0, row, column, player)) return true;
+	if (!is_row_or_column_safe(0, 1, row, column, player)) return true;
+	if (!is_row_or_column_safe(0, -1, row, column, player)) return true;
+
+	// check diagonal moves for attack from bishops, quuens and pawns
+	if (!is_diagonal_safe(1, 1, row, column, player)) return true;
+	if (!is_diagonal_safe(-1, 1, row, column, player)) return true;
+	if (!is_diagonal_safe(1, -1, row, column, player)) return true;
+	if (!is_diagonal_safe(-1, -1, row, column, player)) return true;
+
+	return false;
+}
+
+// checks from kings positions all possible locations where king could be attacked
+// when piece is moved.
+bool GameState::is_king_safe(Move& m, int player) const {
+	int row, column;
+	bool in_board;
+	bool is_king = false;
+	int opponent;
+	if (player == WHITE)
+	{
+		row = _wK_pos[0];
+		column = _wK_pos[1];
+		opponent = BLACK;
+	}
+	else
+	{
+		row = _bK_pos[0];
+		column = _bK_pos[1];
+		opponent = WHITE;
+	}
+
+	if (!is_safe_from_knight(row, column, player)) return false;
+	
+	// check if moved piece is king.
+	// if not king we can avoid a lot of checks
+	if (m._end_pos[0] == row && m._end_pos[1] == column) is_king = true;
+
+	if (!is_king) 
+	{
+		// if not king we can reduce checks by calculating direction that needs to be checked as we
+		// can assume position prior to the move was legal and only the moved piece could open king 
+		// up to danger.
+		int column_dir = 0;
+		int row_dir = 0;
+
+		// if moved piece was on same row or column check for rooks/queens on that file
+		if (m._start_pos[0] == row || m._start_pos[1] == column)
+		{
+			// column direction that needs to be checked, 0 if moved piece was not on the same row
+			if (m._start_pos[0] == row) column_dir = (m._start_pos[1] < column) ? -1 : 1;
+			// row direction, 0 if moved piece was not on the same column
+			if (m._start_pos[1] == column) row_dir = (m._start_pos[0] < row) ? -1 : 1;
+			
+			return is_row_or_column_safe(row_dir, column_dir, row, column, player);
+		}
+
+		// row and column difference of moved pieces original position and king
+		int row_difference = m._start_pos[0] - row;
+		int column_difference = m._start_pos[1] - column;
+
+		// bitwise operation to get absolute value of a number
+		uint32_t temp = row_difference >> 31;
+		row_difference ^= temp;
+		row_difference += temp & 1;
+
+		temp = column_difference >> 31;
+		column_difference ^= temp;
+		column_difference += temp & 1;
+
+		// if column and row difference are same piece was originally diagonally aligned with king
+		if (row_difference == column_difference)
+		{
+			// column direction that needs to be checked
+			column_dir = (m._start_pos[1] < column) ? -1 : 1;
+			// row direction,
+			row_dir = (m._start_pos[0] < row) ? -1 : 1;
+			return is_diagonal_safe(row_dir, column_dir, row, column, player);
+		}
+
+		// if moved piece was not vertically, horizontally or diagonally aligned with king we know king is safe
+		// this is most likely outcome which allows us to skip most of the looping through the board
+		return true;
+	}
+
+	// for king moves we do expensive function call
+	return !is_king_in_check(player);
 }
 
 void GameState::create_promotion_moves(
